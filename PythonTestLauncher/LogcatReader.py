7f41cbcd0cb627fffa771a696f4644a12054bf6e
Teddy <richardgeorgeoff@gmail.com>
2023-03-06 17:40:19 -0800

    Added Parsing and averaging Logs
    Full run through of a test scene working
    
    ## Started running test for: Passthrough
    #Clearing adb logs
    #Installing And Starting App: com.Meta.PerfTesting
    SHELL: adb install ../UnityProjects/Passthrough/Builds/Passthrough/Passthrough.apk
    SHELL: adb shell am start -S com.Meta.PerfTesting/com.unity3d.player.UnityPlayerActivity
    #Waiting for app to load and run: com.Meta.PerfTesting
    #Collecting Logs
    #Parcing Logs
    Avg GPU% usage: 0.47181818181818175
    Avg CPU% usage: 0.16363636363636366

--- PythonTestLauncher/LogcatReader.py
+++ PythonTestLauncher/LogcatReader.py
@@ -1,51 +1,48 @@
-import queue
-import subprocess
-import threading
+import re
 
-#03-06 23:06:59.005  2029  7905 I VrApi   : FPS=90/90,Prd=0ms,Tear=0,Early=0,Stale=0,Stale2/5/10/max=0/0/0/0,VSnc=0,
-# Lat=-1,Fov=0,CPU4/GPU=4/4,1478/525MHz,OC=FF,TA=0/70/0,SP=N/F/N,Mem=2092MHz,Free=5350MB,PLS=0,Temp=31.0C/0.0C,TW=0.63ms,
-# App=0.48ms,GD=0.00ms,CPU&GPU=0.74ms,LCnt=1(DR90,LM0),GPU%=0.12,CPU%=0.02(W0.05),DSF=1.00,CFL=14.67/19.84,LD=0
-class AsynchronousFileReader(threading.Thread):
-    '''
-    Helper class to implement asynchronous reading of a file
-    in a separate thread. Pushes read lines on a queue to
-    be consumed in another thread.
-    '''
+class ParsedLogs:
+    def __init__(self, gpuP, cpuP):
+        self.gpuP = gpuP
+        self.cpuP = cpuP
 
-    def __init__(self, fd, queue):
-        assert isinstance(queue, queue.Queue)
-        assert callable(fd.readline)
-        threading.Thread.__init__(self)
-        self._fd = fd
-        self._queue = queue
+def parseLogs(logs):
+    # get relevant lines from log
+    parsedLines = []
+    lines = logs.split('\n')
+    for line in reversed(lines):
+        #only take last 10 logs of game running to average
+        if len(parsedLines) > 10:
+            break
+        if "CPU&GPU=" in line:
+            gpuP = line.split(",GPU%=", maxsplit=1)[1].split(",", 1)[0]
+            cpuP = re.split(',',line.split(",CPU%=", maxsplit=1)[1].replace("(", ","))[0]
+            # for each line, make a parsed log
+            parsedLines.append(ParsedLogs(gpuP, cpuP))
+    # for each parsed log, get the average over all of them or the last 10 seconds, whichever is less
+    totalGpu = 0
+    totalCpu = 0
+    for plog in parsedLines:
+        totalGpu += float(plog.gpuP)
+        totalCpu += float(plog.cpuP)
+    #return average ParsedLogs object
+    finalAvg = ParsedLogs(totalGpu/len(parsedLines),totalCpu/len(parsedLines))
+    return finalAvg
 
-    def run(self):
-        '''The body of the tread: read lines and put them on the queue.'''
-        for line in iter(self._fd.readline, ''):
-            self._queue.put(line)
 
-    def eof(self):
-        '''Check whether there is no more content to expect.'''
-        return not self.is_alive() and self._queue.empty()
-
-def is_fps_line(line):
-    return True
-
-def update_fps(line):
-    print(line)
-
-def start_logger():
-    # You'll need to add any command line arguments here.
-    process = subprocess.Popen(["adb logcat -s VrApi"], stdout=subprocess.PIPE)
-
-    # Launch the asynchronous readers of the process' stdout.
-    stdout_queue = queue.Queue()
-    stdout_reader = AsynchronousFileReader(process.stdout, stdout_queue)
-    stdout_reader.start()
-
-    # Check the queues if we received some output (until there is nothing more to get).
-    while not stdout_reader.eof():
-        while not stdout_queue.empty():
-            line = stdout_queue.get()
-            if is_fps_line(line):
-                update_fps(line)
+def getTestString():
+    return """03-07 00:43:30.005  2029 12974 I VrApi   : FPS=90/90,Prd=31ms,Tear=0,Early=0,Stale=0,Stale2/5/10/max=0/0/0/0,VSnc=0,Lat=-1,Fov=0,CPU4/GPU=4/4,1478/525MHz,OC=FF,TA=0/70/0,SP=N/F/N,Mem=2092MHz,Free=5284MB,PLS=0,Temp=30.7C/0.0C,TW=2.02ms,App=6.27ms,GD=0.13ms,CPU&GPU=8.58ms,LCnt=4(DR0,LM3),GPU%=0.79,CPU%=0.16(W0.29),DSF=1.00,CFL=14.65/19.92,LD=0
+03-07 00:43:31.004  2029 12974 I VrApi   : FPS=90/90,Prd=30ms,Tear=0,Early=0,Stale=0,Stale2/5/10/max=0/0/0/0,VSnc=0,Lat=-1,Fov=0,CPU4/GPU=4/4,1478/525MHz,OC=FF,TA=0/70/0,SP=N/F/N,Mem=2092MHz,Free=5284MB,PLS=0,Temp=30.7C/0.0C,TW=2.02ms,App=6.27ms,GD=0.13ms,CPU&GPU=9.08ms,LCnt=4(DR0,LM3),GPU%=0.79,CPU%=0.15(W0.25),DSF=1.00,CFL=14.73/20.14,LD=0
+03-07 00:43:32.003  2029 12974 I VrApi   : FPS=90/90,Prd=30ms,Tear=0,Early=0,Stale=0,Stale2/5/10/max=0/0/0/0,VSnc=0,Lat=-1,Fov=0,CPU4/GPU=4/4,2342/525MHz,OC=FF,TA=0/70/0,SP=N/F/N,Mem=2092MHz,Free=5284MB,PLS=0,Temp=30.7C/0.0C,TW=2.02ms,App=6.30ms,GD=0.14ms,CPU&GPU=9.34ms,LCnt=4(DR0,LM3),GPU%=0.79,CPU%=0.17(W0.29),DSF=1.00,CFL=14.58/19.51,LD=0
+03-07 00:43:32.654  2029  2239 D VrApi   : targetSDKVersion 29
+03-07 00:43:32.666  2029 12974 I VrApi   : FPS=90/90,Prd=30ms,Tear=0,Early=0,Stale=0,Stale2/5/10/max=0/0/0/0,VSnc=0,Lat=-1,Fov=0,CPU4/GPU=4/4,1574/525MHz,OC=FF,TA=0/70/0,SP=N/F/N,Mem=2092MHz,Free=5284MB,PLS=0,Temp=30.7C/0.0C,TW=2.01ms,App=6.38ms,GD=0.12ms,CPU&GPU=9.59ms,LCnt=4(DR0,LM3),GPU%=0.80,CPU%=0.12(W0.26),DSF=1.00,CFL=14.65/19.45,LD=0
+03-07 00:43:32.717  2029  2239 I VrApi   : OVR::Stats thread stopped
+03-07 00:43:32.835 24737  3946 D VrApi   : targetSDKVersion 29
+03-07 00:43:32.876 24737  3946 I VrApi   : OVR::Stats thread stopped
+03-07 00:43:33.802 13334 13355 D VrApi   : targetSDKVersion 30
+03-07 00:43:33.826 13334 13355 I VrApi   : OVR::Stats thread started
+03-07 00:43:33.827 13334 13409 D VrApi   : targetSDKVersion 30
+03-07 00:43:33.832 13334 13409 D VrApi   : VrApi::isWifiConnected exception: Make sure android.permission.ACCESS_NETWORK_STATE is set in the manifest.
+03-07 00:43:35.009 13334 13409 I VrApi   : FPS=1/72,Prd=0ms,Tear=0,Early=0,Stale=4,Stale2/5/10/max=0/0/0/0,VSnc=0,Lat=-1,Fov=0,CPU4/GPU=4/4,1478/525MHz,OC=FF,TA=0/0/0,SP=N/N/N,Mem=2092MHz,Free=5115MB,PLS=0,Temp=30.7C/0.0C,TW=1.42ms,App=-110.70ms,GD=0.14ms,CPU&GPU=0.00ms,LCnt=2(DR71,LM2),GPU%=0.21,CPU%=0.02(W0.05),DSF=1.00,CFL=19.70/21.44,LD=0
+03-07 00:43:36.004 13334 13409 I VrApi   : FPS=72/72,Prd=24ms,Tear=0,Early=0,Stale=8,Stale2/5/10/max=0/0/1/30,VSnc=0,Lat=-1,Fov=0,CPU4/GPU=4/4,1478/525MHz,OC=FF,TA=0/0/0,SP=N/N/N,Mem=1353MHz,Free=5115MB,PLS=0,Temp=30.7C/0.0C,TW=1.43ms,App=0.84ms,GD=0.12ms,CPU&GPU=2.24ms,LCnt=3(DR72,LM3),GPU%=0.20,CPU%=0.15(W0.17),DSF=1.00,CFL=19.61/21.52,LD=0
+03-07 00:43:37.002 13334 13409 I VrApi   : FPS=72/72,Prd=24ms,Tear=0,Early=0,Stale=0,Stale2/5/10/max=0/0/0/0,VSnc=0,Lat=-1,Fov=0,CPU4/GPU=4/4,1478/525MHz,OC=FF,TA=0/0/0,SP=N/N/N,Mem=1353MHz,Free=5115MB,PLS=0,Temp=30.7C/0.0C,TW=1.43ms,App=0.88ms,GD=0.12ms,CPU&GPU=2.23ms,LCnt=3(DR72,LM3),GPU%=0.20,CPU%=0.03(W0.05),DSF=1.00,CFL=19.69/21.41,LD=0
+"""
--- PythonTestLauncher/main.py
+++ PythonTestLauncher/main.py
@@ -2,6 +2,7 @@ import os
 import time as t
 import argparse
 
+import LogcatReader
 import OCHelper
 from ConfigReader import *
 from OCHelper import *
@@ -12,11 +13,8 @@ def install_app(appPath):
     os.system("echo Hello from the other side!")
     installAndStartApp(appPath)
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-    timeoutPerAppSeconds = 120
     parser = argparse.ArgumentParser(description="SDK perf test runner", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-p", "--pin", help="pin of logged in user")
     parser.add_argument("-t", "--timeout", help="App max time (seconds) to run before timeout. Default = 120")
@@ -41,16 +39,17 @@ if __name__ == '__main__':
         #give some time for app to boot and collect some logs
         # wait until app is done? - Read a log and until it prints some string its not done
         print(f"#Waiting for app to load and run: {testConfig.packageName}")
-        t.sleep(5)
+        t.sleep(20)
         # capture logs
         print(f"#Collecting Logs")
-        print(getVrAPILogcat().stdout)
-        print(getVrAPILogcat().stderr)
+        logs = getVrAPILogcat().stdout
         # parse the logs
         print(f"#Parcing Logs")
-        #TODO
+        parsedLogs = parseLogs(logs)
         # print results
-        # TODO
+        print(f"Avg GPU% usage: {parsedLogs.gpuP}")
+        print(f"Avg CPU% usage: {parsedLogs.cpuP}")
+        print(getVrAPILogcat().stderr)
         # close and clear app
         print(f"#Closing and removing app")
         closeAndClearApp(testConfig.packageName)
